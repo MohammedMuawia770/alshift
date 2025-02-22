e3cd19482c38651e069356c5ea426e5c333991fc
moham <mohammedalshift@gmail.com>
2025-02-22 15:54:59 +0530

    resolve Split and finshing the project

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 def revers_word(sentence):
-    words = " ".split(sentence)
+    words = sentence.split(" ")
 
     new_word = [word[::-1] for word in words]
 
+    rev = " ".join(new_word)
+    return rev
+
+name = "mohammed muawia awadalla mostafa"
+print(name)
