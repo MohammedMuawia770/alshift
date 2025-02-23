acecd622f44dab96904b86d0dfd6b021c75e73be
moham <mohammedalshift@gmail.com>
2025-02-23 10:38:18 +0530

    code for removing numbers that greater than 50

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
-def revers_word(sentence):
-    words = sentence.split(" ")
+number_list = [10,20,30,40,50,60,70,80,90,100]
+# get list's size
+x = len(number_list)
+i = 0
+# iterate list till i is smaller than x
+while i < x:
+    # check if number is greater than 50
+    if number_list[i] > 50:
+        # delete current index from list
+        del number_list[i]
+        # reduce the list size
+        x = x -1
+    else:
+        # move to next item
+        i+=1
 
-    new_word = [word[::-1] for word in words]
-
-    rev = " ".join(new_word)
-    return rev
-
-name = "mohammed muawia awadalla mostafa"
-print(name)
+print(number_list)
