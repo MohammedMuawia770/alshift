7abf2074f53a3c5bac0e7826b62f6f6ea2e88703
moham <mohammedalshift@gmail.com>
2025-02-25 15:32:46 +0530

    change

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
-number_list = [10,20,30,40,50,60,70,80,90,100]
-# get list's size
-x = len(number_list)
-i = 0
-# iterate list till i is smaller than x
-while i < x:
-    # check if number is greater than 50
-    if number_list[i] > 50:
-        # delete current index from list
-        del number_list[i]
-        # reduce the list size
-        x = x -1
-    else:
-        # move to next item
-        i+=1
+number_list = [10,20,30,40,500,60,70,80,90,10]
 
-print(number_list)
+def larger(number):
+    max = number[0]
+    
+    for i in number:
+        if i > max:
+            max = i
+
+    return max
+
+
+print(larger(number_list))
+
