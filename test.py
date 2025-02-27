9559549be57849a3526389fd75a1b903a9e984f7
moham <mohammedalshift@gmail.com>
2025-02-27 18:00:33 +0530

    i did something that i don't know

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-number_list = ['aba', 'xyz', 'aba', '1221']
-
-def count_string(list):
-    count = 0
-    for word in list:
-        if len(word) > 1 and word[0] == word[-1]:
-
-            count+=1
-
-    return count
-
-        
-
-result = count_string(number_list)
-print(result)
+pre = 0
+for i in range(10):
+    sum = i + pre
+    print("Current Number ",i,"Previous Number ",pre,"sum :",sum)
+    pre = i
